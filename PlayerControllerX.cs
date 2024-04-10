b04b43d4c4fd445c83959cd684541740f38dd9d7
FurkanEfeGithub <166522460+FurkanEfeGithub@users.noreply.github.com>
2024-04-10 14:01:42 +0300

    Create PlayerControllerX.cs

---
+++ PlayerControllerX.cs
@@ -0,0 +1,44 @@
+//BU KOMUT DOSYAMIZIN(PlayerControllerX) HANGI PAKETLERI KULLANDIGINI SOYLER.
+//USING(KULLANMAK),
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//UNITY MonoBehavior SINIFI,HER BIR UNITY BILESENININ TURETILDIGI TEMEL BIR SINIFTIR.
+//BIR BILESEN MonoBehavior SINIFINDAN TURETILMEK ZORUNDADIR.
+//PUBLIC(HERKESE ACIK) CLASS(SINIF) PlayerControllerX(KOMUT DOSYAMIZIN ADI) : MonoBehaviour(UNITY SINIFI)
+public class PlayerControllerX : MonoBehaviour
+{
+    //HIZ DEGISKENI
+    //PUBLIC(HERKESE ACIK) FLOAT(ONDALIKLI SAYI) SPEED(HIZ DEGISKENIMIZIN ADI) = 5.0f(FLOAT(ONDALIKLI SAYI))
+    public float speed = 5.0f;
+    //DONUS HIZI DEGISKENI
+    //PUBLIC(HERKESE ACIK) FLOAT(ONDALIKLI SAYI) ROTATIONSPEED(DONUS HIZI(DEGISKENIMIZIN ADI)
+    public float rotationSpeed;
+    //DIKEY GIRIS DEGISKENI
+    //PUBLIC(HERKESE ACIK) FLOAT(ONDALIKLI SAYI) VERTICALINPUT((DIKEYGIRIS)DEGISKENIMIZIN ADI);
+    public float verticalInput;
+
+    void Update()
+    {
+        //VerticalInput(DIKEY GIRIS DEGISKENIMIZ) = Input(GIRIS YONETICISI).GetAxis[EKSENI AL]("Vertical(DIKEY EKSEN)");
+        verticalInput = Input.GetAxis("Vertical");
+
+        //UCAGIMIZI ILERI DOGRU HAREKET ETTIRECEK KOD SATIRI.
+        //transform(ARACIN TRANSFORMU).Translate(TASIMAK,YER DEGISTIRMEK)(VECTOR3(3D(x,y,z)).FORWARD(ILERI) * Time.deltaTime(ZAMAN FARKI)* SPEED(HIZ DEGISKENI));
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        //------------------------------------------------------------------------
+        //UCAGIN OYUNUMDA SAGA VE SOLA GITMESINE GEREK YOK.
+        //transform.Translate(Vector3.right * Time.deltaTime * rotationSpeed);
+        //-----------------------------------------------------------------
+        //ANAHTAR KELIMELER HAYATIMIZI KOLAYLASTIRIR: ORN:FORWARD(ILERI),RIGHT(SAG),UP(YUKARI)
+        //ASAGI YON TUSUNA BASTIGIMDA UCAK YUKARI,YUKARI YON TUSUNA BASTIGIMDA UCAK ASAGI GIDIYORDU.ISTEDIGIMIN TAM TERSI HAREKET VARDI.
+        ////GERI BASTIGINDA "OK TUSUNA" VEYA "S"(S TUSU) ILERI GIRIS NEGATIFE(-1) DONER.
+        //ILERI BASTIGINDA "OK TUSU" VEYA "W"(W TUSU) ILERI GIRIS POZITIFE(+1) DONER.
+        //COZUM:EGER BEN -1 ILE CARPARSAM.ASAGI YON TUSUNA BASTIGIMDA UCAK ASAGI,YUKARI YON TUSUNA BASTIGIMDA UCAK YUKARI GIDER..ISTEDIGIM DE TAM BU.
+        //ORN:ASAGI YON TUSUNA BASTIGIMDA VERTICALINPUT -1 DEGERINI ALIR.EGER BEN BIR DAHA -1 ILE CARPARSAM -*-= + YAPACAGI ICIN.BU SORUNU COZMUS OLURUM.
+        //----------------------------------------------------------------------------------------------------------
+        //transform(ARACIN TRANSFORMU).Rotate(DONMEK)(VECTOR3(3D(x,y,z)).RIGHT(SAG) * Time.deltaTime(ZAMAN FARKI)* ROTATIONSPEED(DONUSHIZI DEGISKENI) * verticalInput(DIKEY GIRIS DEGISKENI) * -1(EKSI*EKSI=ARTI MANTIGI));
+        transform.Rotate(Vector3.right * Time.deltaTime * rotationSpeed * verticalInput * -1);
+    }
+}
